474b02d0b3d254f8592d850d0b3d906e28e0e380
FadyMagdy-ios <77287109+FadyMagdy-ios@users.noreply.github.com>
2021-01-11 19:24:26 +0200

    Add files via upload
    initial commit

---
+++ Quizzler/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler
+//  Created by Fady Magdy  on 21/12/2020.
+
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
